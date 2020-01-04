003cbb5a5f481e6df5ae5a60ef16c750f6967ff5
Kun Ran <rankun203@gmail.com>
2020-01-04 18:36:11 +0800

    chore: remove console.log

--- src/components/project-gallery/project-card.js
+++ src/components/project-gallery/project-card.js
@@ -28,7 +28,6 @@ export const ProjectCard = ({ ...props }) => {
   const [hoverRef, isHovered] = useHover()
   const videoRef = React.useRef()
 
-  console.log("hover", hoverRef, isHovered)
   React.useEffect(() => {
     if (isHovered) {
       videoRef.current.play()
