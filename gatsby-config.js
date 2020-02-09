4d699f6ccc2f7cd6434a76ed8b92cdf1c07d9a6b
Kun Ran <rankun203@gmail.com>
2020-02-09 20:47:05 +0800

    feat: tooltip styling

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,6 +71,11 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        precachePages: [`/about/`, `/projects/*`],
+      },
+    },
   ],
 }
--- src/components/click-to-copy-link.js
+++ src/components/click-to-copy-link.js
@@ -1,14 +1,26 @@
 import React from "react"
 import { CopyToClipboard } from "react-copy-to-clipboard"
 import styled from "styled-components"
+import IconSuccess from "../images/icon_success.inline.svg"
 
-const Tooltip = styled.span`
-  position: absolute;
+const Tooltip = styled.div`
+  position: fixed;
+  top: 0;
+  left: calc(50% - 100px);
+  width: 200px;
+  height: 40px;
+  line-height: 40px;
+  background-color: rgba(228, 238, 238, 1);
+  color: rgba(33, 119, 119, 1);
   transform: translateY(0);
 
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
   &.animate {
-    transition: all 0.2s;
-    transform: translateY(-10px);
+    transition: all 0.5s;
+    /* transform: translateY(-10px); */
   }
 `
 
@@ -36,6 +48,7 @@ export const ClickToCopyLink = ({
   return (
     <>
       <Tooltip ref={tooltipRef} style={{ opacity: showTooltip ? 1 : 0 }}>
+        <IconSuccess style={{ marginRight: 8 }} />
         {successText}
       </Tooltip>
       <CopyToClipboard text={link.linkLink}>
