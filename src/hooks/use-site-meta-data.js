99bff996ee24b624dc2c950f7cd880d4eafab9c9
Kun Ran <rankun203@gmail.com>
2020-02-09 18:18:52 +0800

    fix: cannot query site metadata

--- src/hooks/use-site-meta-data.js
+++ src/hooks/use-site-meta-data.js
@@ -2,23 +2,22 @@ import { useStaticQuery, graphql } from "gatsby"
 
 /** @deprecated Use hooks/graphql/use-site-meta-data instead */
 export const useSiteMetadata = () => {
-  const { site } = useStaticQuery(
-    graphql`
-      query SiteMetaData {
-        site {
-          siteMetadata {
-            title
-            description
-            author
-            menuLinks {
-              name
-              link
-            }
-          }
-        }
-      }
-    `
-  )
-
-  return site.siteMetadata
+  // const { site } = useStaticQuery(
+  //   graphql`
+  //     query SiteMetaData {
+  //       site {
+  //         siteMetadata {
+  //           title
+  //           description
+  //           author
+  //           menuLinks {
+  //             name
+  //             link
+  //           }
+  //         }
+  //       }
+  //     }
+  //   `
+  // )
+  // return site.siteMetadata
 }
