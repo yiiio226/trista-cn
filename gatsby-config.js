1f00fa04f612407060444583cc595bb6f8387bf9
Kun Ran <rankun203@gmail.com>
2020-02-11 02:14:43 +0800

    chore: remove comments

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,8 +70,6 @@ module.exports = {
         icon: `src/images/favicon.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-remove-serviceworker`,
   ],
 }
