842f19f0066e85230f320c20453c2de0fceeb76f
Kun Ran <rankun203@gmail.com>
2020-02-11 00:13:30 +0800

    refactor: responsive project details page img margin

--- src/templates/project-template.js
+++ src/templates/project-template.js
@@ -118,6 +118,10 @@ const PostBody = styled.div`
     object-fit: scale-down;
     background-color: #ffffff;
     box-shadow: 0px 10px 30px -2px rgba(0, 0, 0, 0.06);
+
+    @media (max-width: 780px) {
+      margin: 30px 0;
+    }
   }
 `
 
