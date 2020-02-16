f34c4d4d6aab95d0103b4721acc7db74d966ae02
Kun Ran <rankun203@gmail.com>
2020-02-17 00:07:05 +0800

    fix: text being cut out

--- src/components/slide-in/children-animated.js
+++ src/components/slide-in/children-animated.js
@@ -2,7 +2,7 @@ import styled from "styled-components"
 
 export const ChildrenAnimated = styled.div`
   white-space: nowrap;
-  height: 64px;
+  height: 72px;
   transform: translateX(${props => props.shiftWidth}px);
   transition-property: transform;
 `
