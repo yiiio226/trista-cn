a8098afb0fb6c10187504dd4faadaa5e9dae5f82
rankun203 <rankun203@users.noreply.github.com>
2020-09-21 12:49:47 +0000

    deployed via Gatsby Publish Action 🎩 for 37d35e82421840ea5ed143eab8b3daebc3a22e5a

---
+++ component---src-pages-404-js-0eadf60ba626fea9c64b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),u=n("Kvkj");t.default=function(){return l.a.createElement(l.a.Fragment,null,l.a.createElement(u.m,{title:"404: Not found"}),l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-0eadf60ba626fea9c64b.js.map
